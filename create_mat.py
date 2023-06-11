14136df316cfcf5f2d0f7070287261b342e2bc9d
Irsnag <104862524+Irsnag@users.noreply.github.com>
2023-06-11 22:57:26 +0200

    Add files via upload

---
+++ create_mat.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  4 22:27:58 2023
+
+@author: 33606
+"""
+
+import numpy as np
+import re
+
+# Function to correct the file
+def correct_file(filename):
+    """
+    Corrects the output txt file from JM Master by updating the macroblock coordinates.
+
+    Args:
+        filename (str): The path of the file to be corrected.
+
+    """
+    
+    n = -1
+    lines = []
+    
+    # Read the file
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line)
+    
+    # Iterate through the lines
+    for i in range(len(lines)):
+        # Check if the line indicates the start of a new macroblock
+        if lines[i] == 'Macroblock (0,0,0):\n':
+            n += 1
+        # Check if the line contains the macroblock coordinates
+        if 'Macroblock (0' in lines[i]: 
+            # Correct the macroblock coordinates by replacing them with the updated value of 'n'
+            lines[i] = 'Macroblock (' + str(n) + lines[i][13:]
+    
+    # Write the corrected lines to a new file
+    with open('corr_' + filename, 'w') as c:
+        for line in lines:
+            c.write(line)
+
+
+
+
+# Function to create a matrix from the file
+def create_matrix(filename):
+    
+    """
+    Creates a matrix from the given file.
+
+    Args:
+        filename (str): The name of the file.
+
+    Returns:
+        numpy.ndarray: The matrix created from the file.
+
+    """
+    
+    last_tuple = None
+    all_tuples = []
+    lines = []
+    
+    # Read the file
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line)
+            tuples = re.findall(r'\([^\)]*\)', line)
+
+            if tuples:
+                last_tuple = tuples[-1]
+                all_tuples.append(tuples)
+    
+    # Extract the dimensions of the matrix from the last tuple
+    t = tuple(int(x) + 1 for x in last_tuple.strip('()').split(','))
+    
+    # Process the tuples
+    for n in range(len(all_tuples)):
+        all_tuples[n] = tuple(int(x) for x in all_tuples[n][0].strip('()').split(','))
+    
+    # Create a matrix with zeros
+    M = np.zeros((t[0], 16 * t[2], 16 * t[1]))
+    
+    nb_block = t[0] * t[1] * t[2]
+    
+    # Iterate through the blocks
+    for n in range(nb_block):
+        current_index = all_tuples[n]
+        
+        # Extract data for each block from the lines
+        data = [list(map(int, line.strip().split())) for line in lines[17 * n + 1:17 * (n + 1)]]
+        
+        matrix = np.array(data)
+        
+        # Assign the block matrix to the corresponding position in the overall matrix
+        M[current_index[0], current_index[2] * 16:current_index[2] * 16 + 16, current_index[1] * 16:current_index[1] * 16 + 16] = matrix
+    
+        M=M[:,:,8:712]
+    
+    
+    return M
+
+
+
+
+
+# Uncomment the following lines if you want to execute the functions
+
+# filename = 'input.txt'
+# correct_file(filename)
+# matrix = create_matrix('corr_' + filename)
+
+    
+
+
+
+
---
+++ filtrage.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  2 12:34:31 2023
+
+@author: 33606
+"""
+
+import numpy as np
+
+def list_filtrage(label):
+    """
+    Filter a label list and return the indices of images with all-zero labels.
+
+    Args:
+        label (numpy.ndarray): List of labels with shape (num_images, height, width).
+
+    Returns:
+        list: List of indices corresponding to images with all-zero labels.
+    """
+
+    # Calculate the number of images
+    nb_images = np.shape(label)[0]
+
+    # Get the dimensions of the label
+    dim = np.shape(label)[1], np.shape(label)[2]
+
+    # Initialize an empty list
+    L = []
+
+    # Reshape the label array
+    M_bis = np.reshape(label, (nb_images, dim[0], dim[1], 1))
+
+    # Iterate over the images
+    for i in range(nb_images):
+        # Check if any value in the image is non-zero
+        if not np.any(M_bis[i, :, :, :]):
+            # Append the index to the list
+            L.append(i)
+
+    return L
+
+
+def filtrage(data, label, L):
+    """
+    Filter data and labels based on the indices to keep.
+
+    Args:
+        data (numpy.ndarray): Data array with shape (num_images, height, width).
+        label (numpy.ndarray): Label array with shape (num_images, height, width).
+        L (list): List of indices to keep.
+
+    Returns:
+        numpy.ndarray: Filtered data array.
+        numpy.ndarray: Filtered label array.
+    """
+
+    # Calculate the number of images
+    nb_images = np.shape(label)[0]
+
+    # Get the dimensions of the label
+    dim = np.shape(label)[1], np.shape(label)[2]
+
+    # Reshape the label array
+    label = np.reshape(label, (nb_images, dim[0], dim[1]))
+
+    # Initialize new arrays for filtered data and labels
+    data_f = np.zeros((nb_images - len(L), dim[0], dim[1]))
+    label_f = np.zeros((nb_images - len(L), dim[0], dim[1]))
+
+    # Initialize a counter
+    j = 0
+
+    # Iterate over the images
+    for i in range(nb_images):
+        # Check if the index is not in the list of indices to exclude
+        if i not in L:
+            # Copy the data and label to the filtered arrays
+            data_f[j, :, :] = data[i, :, :]
+            label_f[j, :, :] = label[i, :, :]
+            j += 1
+
+    return data_f, label_f
